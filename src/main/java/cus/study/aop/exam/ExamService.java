d4fe8b060127835d9304cd2e8bd42f6f6d0313e1
ungseokchoi <cus4703@gmail.com>
2022-02-24 17:54:30 +0900

    feat: simple service retry example create

---
+++ src/main/java/cus/study/aop/exam/ExamService.java
@@ -0,0 +1,16 @@
+package cus.study.aop.exam;
+
+import cus.study.aop.exam.annotation.Trace;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ExamService {
+    private final ExamRepository examRepository;
+
+    @Trace
+    public void request(String itemId) {
+        examRepository.save(itemId);
+    }
+}
---
+++ src/test/java/cus/study/aop/exam/ExamTest.java
@@ -0,0 +1,23 @@
+package cus.study.aop.exam;
+
+import cus.study.aop.exam.aop.RetryAspect;
+import cus.study.aop.exam.aop.TraceAspect;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+//@Import(TraceAspect.class)
+@Import({TraceAspect.class, RetryAspect.class})
+@SpringBootTest
+public class ExamTest {
+    @Autowired
+    ExamService examService;
+
+    @Test
+    void test() {
+        for (int i = 0; i < 5; i++) {
+            examService.request("data" + i);
+        }
+    }
+}
